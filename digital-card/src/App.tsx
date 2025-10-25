29dd3d4d5f10c3151c9271b48740d081e31a3128
mwape-k <82158230+mwape-k@users.noreply.github.com>
2025-10-25 02:48:04 +0200

    finish

--- digital-card/src/App.tsx
+++ digital-card/src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Lanyard from "./components/lanyard";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <div
+        className="ditherComponent"
+        style={{ width: "100vw", height: "100%" }}
+      >
+        <Lanyard position={[0, 0, 20]} gravity={[0, -40, 0]} />
+      </div>
+      <div className="spacer"></div>
+      <div className="button-container">
+        <a
+          href="https://mwape-kurete-portfolio.vercel.app/"
+          className="link-btn"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Website
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a
+          href="http://www.linkedin.com/in/mwape-kurete-"
+          className="link-btn"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+        <a
+          href="https://github.com/Mwape-Kurete?tab=overview&from=2025-05-01&to=2025-05-06"
+          className="link-btn"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+        <a
+          href="https://www.behance.net/mwapekurete2"
+          className="link-btn"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Behance
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
